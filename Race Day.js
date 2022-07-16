c92e5e70ed7e7e7ad594c92f46bac4c5f9e82edd
Ashley Chea <60674422+ashleychea@users.noreply.github.com>
2022-07-15 21:23:18 -0400

    Update Race Day.js

--- Race Day.js
+++ Race Day.js
@@ -1,12 +1,16 @@
 let raceNumber = Math.floor(Math.random() * 1000);
 
-const registeredEarly = true;
+let registeredEarly = true;
+// registeredEarly = false; (If you set the variable to equal false and you change the age to > 18, ex. 67), the ouput changes to: Your race starts at 11:00am
 
-let runnersAge = 45;
+// Used the variable const; the runnersAge can be changed at any time for a different output. 
+const runnersAge = 45;
 
+// Set the if statement and added an addition assignment to add the random raceNumber by 1000. 
 if(runnersAge > 18 && registeredEarly) {
     raceNumber+=1000;
-
+    
+// Used else if statments to allow for more than two possible outcomes. 
     console.log(`Runner ${raceNumber}: Your race starts at 9:30am`);
 } else if(runnersAge > 18 && !registeredEarly) {
     console.log('Your race starts at 11:00am');
@@ -15,3 +19,5 @@ if(runnersAge > 18 && registeredEarly) {
 } else if(runnersAge = 18) {
     console.log('Please see the registration desk');
 }
+
+// Output: Runner (random number between 1000-1999): Your race starts at 9:30am.
